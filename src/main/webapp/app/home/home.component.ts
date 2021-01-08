bad431f3cadbb3f03df51831e1f8c85b19674183
hunglv <hunglv>
2021-01-08 16:44:56 +0700

    Add\

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'jhi-home',
@@ -11,13 +12,41 @@ import { Account } from 'app/core/user/account.model';
   styleUrls: ['home.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
+  peliculas: any[] = [
+    {
+      name: '',
+      img: 'content/assets/baner/wall1.png',
+      desc: '',
+    },
+    {
+      name: '',
+      img: 'content/assets/baner/wall2.png',
+      desc: '',
+    },
+    {
+      name: '',
+      img: 'content/assets/baner/wall3.png',
+      desc: '',
+    },
+    {
+      name: '',
+      img: 'content/assets/baner/wall4.png',
+      desc: '',
+    },
+    {
+      name: '',
+      img: 'content/assets/baner/wall5.png',
+      desc: '',
+    },
+  ];
+
   account: Account | null = null;
   authSubscription?: Subscription;
 
-  constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
-
-  ngOnInit(): void {
-    this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
+  constructor(private accountService: AccountService, private loginModalService: LoginModalService, private _config: NgbCarouselConfig) {
+    _config.interval = 3000;
+    _config.pauseOnHover = true;
+    _config.showNavigationArrows = false;
   }
 
   isAuthenticated(): boolean {
@@ -32,5 +61,8 @@ export class HomeComponent implements OnInit, OnDestroy {
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
     }
+  }
+  ngOnInit(): void {
+    this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
   }
 }
