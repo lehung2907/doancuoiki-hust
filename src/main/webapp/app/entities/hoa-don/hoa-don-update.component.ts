1967321d7714dc9b92d6390171bfbeb15fb8f01b
hunglv <hunglv>
2021-01-27 23:44:11 +0700

    Add

--- src/main/webapp/app/entities/hoa-don/hoa-don-update.component.ts
+++ src/main/webapp/app/entities/hoa-don/hoa-don-update.component.ts
@@ -70,6 +70,7 @@ export class HoaDonUpdateComponent implements OnInit {
       id: 3,
       trangThai: 'Giao hàng thành công',
     };
+    //abc123
     const dmTrangThai4 = {
       id: 4,
       trangThai: 'Giao hàng thất bại',
